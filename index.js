c847bf52647a6eb41096a0264b836718cd2618b7
Conary36 <crossingwaters@icloud.com>
2022-01-18 21:44:41 -0500

    created axios promise that stores url and title

--- index.js
+++ index.js
@@ -5,8 +5,27 @@ const cheerio = require('cheerio');
 
 const app = express();
 
+const storage = [];
+
 app.get('/', (req, res)=>{
     res.json('Welcome to my Climate Change News API')
+})
+app.get('/news', (req, res)=>{
+    axios.get('https://www.theguardian.com/us/environment')
+        .then((response)=>{
+            const html = response.data
+            // console.log(html)
+            const $ = cheerio.load(html);
+            $('a:contains("climate")', html).each(function (){
+                const title = $(this).text();
+                const url = $(this).attr('href');
+                storage.push({
+                    title,
+                    url
+                })
+            })
+            res.json(storage);
+        }).catch((err)=> console.log(err));
 })
 app.listen(PORT, () =>
 console.log(`server is running on PORT ${PORT}`));
