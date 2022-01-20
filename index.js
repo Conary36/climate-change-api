ef3376dbe5aa2cd52f1b536fcb6baa608d91097c
Conary36 <crossingwaters@icloud.com>
2022-01-20 00:22:21 -0500

    hardcoded data

--- index.js
+++ index.js
@@ -5,27 +5,64 @@ const cheerio = require('cheerio');
 
 const app = express();
 
+
+const newspapers = [
+    {
+        name: 'thetimes',
+        address: 'https://www.thetimes.co.uk/environment/climate-change'
+    },
+    {
+        name: 'guardian',
+        address: 'https://www.theguardian.com/environment/climate-change'
+    },
+    {
+        name: 'telegraph',
+        address: 'https://www.telegraph.co.uk/climate-change'
+    }
+]
 const storage = [];
 
-app.get('/', (req, res)=>{
-    res.json('Welcome to my Climate Change News API')
-})
-app.get('/news', (req, res)=>{
-    axios.get('https://www.theguardian.com/us/environment')
-        .then((response)=>{
-            const html = response.data
-            // console.log(html)
+newspapers.forEach(newspaper =>{
+    axios.get(newspaper.address)
+        .then(response =>{
+            const html = response.data;
             const $ = cheerio.load(html);
-            $('a:contains("climate")', html).each(function (){
+
+            $('a:contains("climate")', html).each(function(){
                 const title = $(this).text();
                 const url = $(this).attr('href');
                 storage.push({
                     title,
-                    url
+                    url,
+                    source: newspaper.name
                 })
             })
-            res.json(storage);
-        }).catch((err)=> console.log(err));
+        }).catch(err =>console.log(err));
+
+})
+
+app.get('/', (req, res)=>{
+    res.json('Welcome to my Climate Change News API')
+})
+app.get('/newspaper', (reg, res)=>{
+    res.json(storage);
+})
+app.get('/news', (req, res)=>{
+    // axios.get('https://www.theguardian.com/us/environment')
+    //     .then((response)=>{
+    //         const html = response.data
+    //         // console.log(html)
+    //         const $ = cheerio.load(html);
+    //         $('a:contains("climate")', html).each(function (){
+    //             const title = $(this).text();
+    //             const url = $(this).attr('href');
+    //             storage.push({
+    //                 title,
+    //                 url
+    //             })
+    //         })
+    //         res.json(storage);
+    //     }).catch((err)=> console.log(err));
 })
 app.listen(PORT, () =>
 console.log(`server is running on PORT ${PORT}`));
